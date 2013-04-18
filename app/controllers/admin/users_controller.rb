1ac0ac6f1c025f5b4f7ae9d60aabe3d28083dd5d
erlon <erlonlon@gmail.com>
2013-04-18 20:44:10 -0300

    atua01

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,82 +1,48 @@
 class Admin::UsersController < Admin::BaseController
  
+ before_filter :controle_extra
   def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
+    @users = User.paginate page: params[:page], :per_page => 10
+    respond_with @users
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
+    respond_with @user
   end
-
-  # GET /users/1/edit
+  
   def edit
     @user = User.find(params[:id])
   end
-
-  # POST /users
-  # POST /users.json
+ 
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Registro salvo com sucesso !" if @user.save
+    respond_with @user, :location => admin_users_path    
+    
   end
 
   # PUT /users/1
   # PUT /users/1.json
   def update
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Registro salvo com sucesso !" if @user.update_attributes(prams[:user])
+    respond_with @user, :location => admin_users_path   
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+ 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    respond_with @user, :location => admin_users_path    
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+  
+  end
+
+
+  def controle_extra
+
+  @categories = Category.all
+  @galleries = Gallery.paginate page: params[:page], :per_page => 10
   end
 end
